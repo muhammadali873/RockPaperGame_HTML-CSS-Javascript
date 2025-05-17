f6ff29dfab407d5542378e84dc806ebaa3d7c5e6
muhammadali873 <malighori2006@gmail.com>
2025-05-17 19:51:39 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,15 @@
+const profile = {
+    username: "shradhakhapra",
+    isFollow: false,
+    isMessage: true,
+    totalPost: "195",
+    totalFollowers: 569,
+    totalFollowing: "4",
+    profileName: "Shradha Khapra",
+    threadProfile: "@shradhakhapra",
+    profession: "Enterprenuer",
+    tagLine: "Apna College | Ex-Microsoft, DRDO",
+    tagline2: "To educate someone is the highest privilege"
+};
+
+console.log(typeof profile["totalFollowers"]);
